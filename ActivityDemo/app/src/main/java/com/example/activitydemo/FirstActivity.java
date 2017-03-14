e727b8b5799ead18cef2503f1997dc50efda28d1
zhongweichang <15979771469@163.com>
2017-03-14 18:02:32 +0800

    活动示例代码

--- ActivityDemo/app/src/main/java/com/example/activitydemo/FirstActivity.java
+++ ActivityDemo/app/src/main/java/com/example/activitydemo/FirstActivity.java
@@ -1,5 +1,7 @@
 package com.example.activitydemo;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -9,6 +11,23 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class FirstActivity extends AppCompatActivity {
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch(requestCode)
+        {
+            case 1:
+                if (resultCode == RESULT_OK)
+                {
+                    String returnData = data.getStringExtra("data_return");
+
+                    Toast.makeText(this, returnData, Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                break;
+
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +39,48 @@ public class FirstActivity extends AppCompatActivity {
            public void onClick(View v) {
                 Toast.makeText(FirstActivity.this, "you clicked button1!", Toast.LENGTH_SHORT).show();
             }
+        });
+
+        Button button2 = (Button)findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(FirstActivity.this, "destroy activity!", Toast.LENGTH_SHORT).show();
+                finish();
+
+            }
+        });
+
+        Button button3 = (Button)findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //隐式Intent示范
+                Intent intent = new Intent("com.example.activity.ACTION_START");
+                String str = "爱你哈哈";
+                intent.putExtra("data", str);
+                startActivity(intent);
+            }
+        });
+
+        Button button4 = (Button)findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //使用内置Intent
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://www.baidu.com"));
+                startActivity(intent);
+            }
+        });
+
+        Button button5 = (Button)findViewById(R.id.button5);
+        button5.setOnClickListener(new View.OnClickListener(){
+           @Override
+            public void onClick(View v){
+               Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+               startActivityForResult(intent, 1);
+           }
         });
     }
 
@@ -37,7 +98,14 @@ public class FirstActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        Toast.makeText(this, "you clicked " + item.getTitle(), Toast.LENGTH_SHORT).show();
+        if (R.id.second_activity != item.getItemId()) {
+            Toast.makeText(this, "you clicked " + item.getTitle(), Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Intent intent = new Intent(this, SecondActivity.class);
+            startActivity(intent);
+        }
         return true;
     }
 }
---
+++ ActivityDemo/app/src/main/java/com/example/activitydemo/SecondActivity.java
@@ -0,0 +1,34 @@
+package com.example.activitydemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Button button2 = (Button)findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent();
+                intent.putExtra("data_return", "Hello FirstActivity");
+                setResult(RESULT_OK, intent);
+                Toast.makeText(SecondActivity.this, "活动二结束", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
+
+        Intent intent = getIntent();
+        String data = intent.getStringExtra("data");
+        if (!data.isEmpty())
+            Toast.makeText(this, data, Toast.LENGTH_SHORT).show();
+    }
+}
