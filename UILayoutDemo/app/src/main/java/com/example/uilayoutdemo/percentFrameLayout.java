11f3f0112203dfdc28993eeeca29ccb0fc4f6aa9
zhongweichang <15979771469@163.com>
2017-03-15 17:59:39 +0800

    布局示例

---
+++ UILayoutDemo/app/src/main/java/com/example/uilayoutdemo/percentFrameLayout.java
@@ -0,0 +1,13 @@
+package com.example.uilayoutdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class percentFrameLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_percent_frame_layout);
+    }
+}
