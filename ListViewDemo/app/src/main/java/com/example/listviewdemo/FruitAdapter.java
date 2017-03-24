e84e167172ad5263af28aa8bd273e4497f94c7bb
钟惟昌 <15979771469@163.com>
2017-03-24 10:08:41 +0800

    ListView示例

--- ListViewDemo/app/src/main/java/com/example/listviewdemo/FruitAdapter.java
+++ ListViewDemo/app/src/main/java/com/example/listviewdemo/FruitAdapter.java
@@ -19,18 +19,35 @@ public class FruitAdapter extends ArrayAdapter<Fruit> {
     public FruitAdapter(Context context, int textViewResourceId, List<Fruit> objects){
         super(context, textViewResourceId, objects);
         resourceId = textViewResourceId;
+        String str = context.getFilesDir().toString();
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent){
         Fruit fruit = getItem(position);
-        View view = LayoutInflater.from(getContext()).inflate(resourceId, parent, false);
-        ImageView fruitImage = (ImageView)view.findViewById(R.id.fruit_image);
-        TextView fruitName = (TextView)view.findViewById(R.id.fruit_name);
+        View view;
+        ViewHolder viewHolder;
+        if (null == convertView) {
+            view = LayoutInflater.from(getContext()).inflate(resourceId, parent, false);
+            viewHolder = new ViewHolder();
+            viewHolder.fruitImage = (ImageView)view.findViewById(R.id.fruit_image);
+            viewHolder.fruitName = (TextView)view.findViewById(R.id.fruit_name);
+            view.setTag(viewHolder);
+        } else {
+            view = convertView;
+            viewHolder = (ViewHolder)view.getTag();
+        }
+        //ImageView fruitImage = (ImageView)view.findViewById(R.id.fruit_image);
+        //TextView fruitName = (TextView)view.findViewById(R.id.fruit_name);
 
-        fruitImage.setImageResource(fruit.getImageId());
-        fruitName.setText(fruit.getName());
+        viewHolder.fruitImage.setImageResource(fruit.getImageId());
+        viewHolder.fruitName.setText(fruit.getName());
         return view;
     }
 
+
+    class ViewHolder{
+        ImageView fruitImage;
+        TextView fruitName;
+    }
 }
